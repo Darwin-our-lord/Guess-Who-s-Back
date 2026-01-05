7a0125baa674a4e8313ea5ac0a0d58e90e284633
Darwin-our-lord <David@prebensen.dk>
2026-01-05 22:05:58 +0100

    camera pan

--- Guess Who’s Back/Assets/Scripts/CameraController.cs
+++ Guess Who’s Back/Assets/Scripts/CameraController.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public float cameraSpeed;
+    public float panBorderThickness;
 
     void Update()
     {
         float hor = Input.GetAxisRaw("Horizontal");
         float ver = Input.GetAxisRaw("Vertical");
+
+        if (Input.mousePosition.y >= Screen.height - panBorderThickness) { ver =  1; }//top
+        if (Input.mousePosition.y <= panBorderThickness)                 { ver = -1; }//bot
+        if (Input.mousePosition.x >= Screen.width - panBorderThickness)  { hor =  1; }//right
+        if (Input.mousePosition.x <= panBorderThickness)                 { hor = -1; }//left
 
         transform.position += new Vector3(hor, ver, 0).normalized * cameraSpeed * Time.deltaTime;
 
