8878aae5a2ec3b1884f9e84f31e26005f1989dbb
Darwin-our-lord <David@prebensen.dk>
2026-02-16 09:59:50 +0100

    works!!

--- Guess Who’s Back/Assets/Scripts/StoreManager.cs
+++ Guess Who’s Back/Assets/Scripts/StoreManager.cs
@@ -63,6 +63,7 @@ public class StoreManager : MonoBehaviour
                 if (randomValue <= currentWeight)
                 {
                     rarity = option.Item1;
+                    break;
                 }
             }
             while (true)
